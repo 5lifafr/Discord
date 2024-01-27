d8658ab478ce7d0c39b702c779329a9e2642edd0
5lifafr <157732968+5lifafr@users.noreply.github.com>
2024-01-27 17:54:35 +0400

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Asia/Dubai', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -23,19 +23,17 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('993245909079052369')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
-    .setDetails(`Valorant [${formatTime()}]`)
+    .setURL('https://www.twitch.tv/5lifafr') //Must be a youtube video link
+    .setName('/anyy')
+    .setDetails(`kk.`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1181262876003020910/1200385165407555664/1280px-HD_transparent_picture.png?ex=65c5fccd&is=65b387cd&hm=5fffb20b31418e175dc0748997eba0aaf6eef5f21599e56bc98d448eea91f32d&=&format=webp&quality=lossless&width=1111&height=625') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('.gg/anyy') //Text when you hover the Large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1181262876003020910/1200121128161648710/verifiedblack.gif?ex=65c506e6&is=65b291e6&hm=ae9676eb84fcf8b15fbf7846f0c63eb5d739d540a5e0404c3b1cd89024aa4d22&=') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Verfied') //Text when you hover the Small image
+    .addButton('@k', 'https://discord.gg/anyy');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -44,7 +42,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `kk.`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
